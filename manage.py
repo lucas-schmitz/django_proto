88afa3997bded65016c2c89b2d182022124c7d16
lucas-schmitz <l.h.g.schmitz@gmail.com>
2018-08-06 20:25:43 +0200

    changed the manage.py changed the .gitignore file to exclude .idea folder in pycharm

--- manage.py
+++ manage.py
@@ -11,5 +11,10 @@ if __name__ == '__main__':
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )
+    # added by Lucas S.
+    # automatically open the address in firefox
+    if sys.argv[1] == "runserver":
+        os.system("firefox http://127.0.0.1:8000/")
     execute_from_command_line(sys.argv)
+
